cd3b3d58f1660776390ad8218834bdb45bdeb0ff
LeandroRocha <leandro.rocha@sof.to>
2024-05-02 22:06:46 -0300

    CategoryModel

---
+++ lib/category_model.dart
@@ -0,0 +1,64 @@
+import 'package:equatable/equatable.dart';
+
+class CategoryModel extends Equatable {
+  const CategoryModel({
+    required this.id,
+    required this.name,
+    required this.created,
+    required this.limitMonthly,
+    required this.consumed,
+  });
+
+  factory CategoryModel.fromJson(Map<String, dynamic> json) {
+    return CategoryModel(
+      id: json['id'] as String? ?? '',
+      name: json['name'] as String? ?? '',
+      created: DateTime.tryParse(json['created'] as String? ?? ''),
+      limitMonthly: (json['limit_monthly'] as num?)?.toDouble() ?? 0.0,
+      consumed: (json['consumed'] as num?)?.toDouble() ?? 0.0,
+    );
+  }
+
+  final String id;
+  final String name;
+  final DateTime? created;
+  final double limitMonthly;
+  final double consumed;
+
+  CategoryModel copyWith({
+    String? id,
+    String? name,
+    DateTime? created,
+    double? limitMonthly,
+    double? consumed,
+  }) {
+    return CategoryModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      created: created ?? this.created,
+      limitMonthly: limitMonthly ?? this.limitMonthly,
+      consumed: consumed ?? this.consumed,
+    );
+  }
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'id': id,
+        'name': name,
+        'created':
+            "${created?.year.toString().padLeft(4, '0')}-${created?.month.toString().padLeft(2, '0')}-${created?.day.toString().padLeft(2, '0')}",
+        'limit_monthly': limitMonthly,
+        'consumed': consumed,
+      };
+
+  @override
+  String toString() => '$id, $name, $created, $limitMonthly, $consumed, ';
+
+  @override
+  List<Object?> get props => <Object?>[
+        id,
+        name,
+        created,
+        limitMonthly,
+        consumed,
+      ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      // options: DefaultFirebaseOptions.currentPlatform,
-      );
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
@@ -33,9 +35,10 @@ class MyHomePage extends StatelessWidget {
           children: <Widget>[
             ElevatedButton(
               onPressed: () async {
-                FirebaseFirestore.instance
+                firestore
                     .collection('collection')
-                    .add(<String, String>{'date': dateNow});
+                    .doc('doc')
+                    .set(<String, String>{'date': dateNow});
 
                 debugPrint('test $dateNow');
               },
