e9af27a4527c32879ab47b3c6864b5a7fc88ad23
LeandroRocha <leandro.rocha@sof.to>
2024-05-02 22:12:36 -0300

    create Category

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'category_model.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -24,23 +25,24 @@ class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.firestore});
 
   final FirebaseFirestore firestore;
-  String get collection => 'test';
 
   @override
   Widget build(BuildContext context) {
-    final String dateNow = DateTime.now().toIso8601String();
     return SafeArea(
       child: Scaffold(
         body: Column(
           children: <Widget>[
             ElevatedButton(
               onPressed: () async {
-                firestore
-                    .collection('collection')
-                    .doc('doc')
-                    .set(<String, String>{'date': dateNow});
-
-                debugPrint('test $dateNow');
+                firestore.collection('categories').doc('mercado').set(
+                      CategoryModel(
+                              name: 'mercado',
+                              created: DateTime.now(),
+                              limitMonthly: 400,
+                              id: '1',
+                              consumed: 84)
+                          .toJson(),
+                    );
               },
               child: const Text('create'),
             ),
