d9fb19d2b4dcea456713268df1b6f9d9fda5c84d
LeandroRocha <leandro.rocha@sof.to>
2024-05-02 23:02:13 -0300

    modalCreateCategory

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'category_model.dart';
 import 'controller.dart';
 import 'firebase_options.dart';
+import 'text_field_custom_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -57,17 +58,76 @@ class CategoryPage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          controller.createCategory(
-            Category(
-              name: 'mercado',
-              created: DateTime.now(),
-              limitMonthly: 400,
-              consumed: 84,
-            ),
-          );
+          modalCreateCategory(context);
         },
         child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  Future<dynamic> modalCreateCategory(BuildContext context) {
+    final TextEditingController nameEC = TextEditingController();
+    final FocusNode nameFN = FocusNode();
+
+    final TextEditingController limitEC = TextEditingController();
+    final FocusNode limitFN = FocusNode();
+
+    return showModalBottomSheet(
+      context: context,
+      useSafeArea: true,
+      enableDrag: true,
+      showDragHandle: true,
+      isScrollControlled: true,
+      builder: (BuildContext contextModal) {
+        return Wrap(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                children: <Widget>[
+                  TextFieldCustomWidget(
+                    controller: nameEC,
+                    focusNode: nameFN,
+                    hintText: 'Nome da categoria',
+                  ),
+                  const SizedBox(height: 24),
+                  TextFieldCustomWidget(
+                    controller: limitEC,
+                    focusNode: limitFN,
+                    hintText: 'Limite mensal',
+                    keyboardType: TextInputType.number,
+                  ),
+                  const SizedBox(height: 24),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 24),
+                      shape: const StadiumBorder(),
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                      textStyle: Theme.of(context).textTheme.button,
+                    ),
+                    onPressed: () {
+                      controller.createCategory(
+                        Category(
+                          name: nameEC.text,
+                          created: DateTime.now(),
+                          limitMonthly: limitEC.text.isEmpty
+                              ? 0
+                              : double.parse(limitEC.text),
+                          consumed: 0,
+                        ),
+                      );
+                      Navigator.of(contextModal).pop();
+                    },
+                    child: const Text('Criar categoria'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
---
+++ lib/text_field_custom_widget.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class TextFieldCustomWidget extends StatelessWidget {
+  const TextFieldCustomWidget({
+    super.key,
+    required this.focusNode,
+    required this.controller,
+    required this.hintText,
+    this.onTap,
+    this.readOnly = false,
+    this.showSuffixIcon = false,
+    this.keyboardType = TextInputType.text,
+    this.maxLines = 1,
+    this.onChanged,
+    this.inputFormatters,
+  });
+
+  final FocusNode focusNode;
+  final TextEditingController controller;
+  final String hintText;
+  final void Function()? onTap;
+  final bool readOnly;
+  final bool showSuffixIcon;
+  final TextInputType? keyboardType;
+  final int? maxLines;
+  final void Function(String)? onChanged;
+  final List<TextInputFormatter>? inputFormatters;
+
+  @override
+  Widget build(BuildContext context) {
+    final InkWell buttonClear = InkWell(
+      onTap: () {
+        controller.clear();
+        focusNode.requestFocus();
+      },
+      child: const Icon(
+        Icons.close,
+        size: 24,
+        // color: ,
+      ),
+    );
+
+    const Icon iconChevronRight = Icon(
+      CupertinoIcons.chevron_right,
+      size: 24,
+      // color: AppColors.blueyGray,
+    );
+
+    return ValueListenableBuilder<TextEditingValue>(
+      valueListenable: controller,
+      builder: (_, __, ___) {
+        final Widget iconReturn =
+            showSuffixIcon ? iconChevronRight : const SizedBox.shrink();
+        final Widget suffixIcon =
+            controller.text.isNotEmpty ? buttonClear : iconReturn;
+
+        return TextField(
+          controller: controller,
+          readOnly: readOnly,
+          focusNode: focusNode,
+          autocorrect: false,
+          enableSuggestions: false,
+          textInputAction: TextInputAction.done,
+          // style: AppTextStyles.primaryDark16w500,
+          keyboardType: keyboardType,
+          maxLines: maxLines,
+          minLines: maxLines,
+          inputFormatters: inputFormatters,
+          decoration: InputDecoration(
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            filled: true,
+            hintText: hintText,
+            // hintStyle: AppTextStyles.primaryDark16w500,
+            labelText: hintText,
+            // labelStyle: AppTextStyles.text12w500.copyWith(
+            //   color: AppColors.blueFA,
+            // ),
+            // counterStyle: AppTextStyles.primaryDark16w500,
+            // fillColor: AppColors.white,
+            suffixIcon: suffixIcon,
+          ),
+          onTap: onTap,
+          onChanged: onChanged,
+        );
+      },
+    );
+  }
+}
