f49f9869a01c58dc39a579f57a275f62804f42b7
LeandroRocha <leandro.rocha@sof.to>
2024-07-19 18:42:59 -0300

    debts factory

--- lib/core/factories/debt_factory.dart
+++ lib/core/factories/debt_factory.dart
@@ -3,14 +3,31 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../features/debt/debit.dart';
 import 'firestore_factory.dart';
 
-DebtPage makeDebtPage() {
+DebtRepository makeDebtRepository() {
   FirebaseFirestore firestore = makeFirestoreFactory();
   DebtRepository debtRepository = FirebaseDebtRepository(firestore);
+  return debtRepository;
+}
+
+GetDebtsUseCase makeGetDebtsUseCase() {
+  DebtRepository debtRepository = makeDebtRepository();
+  GetDebtsUseCase getDebtsUseCase = GetDebtsUseCase(debtRepository);
+  return getDebtsUseCase;
+}
+
+GetSumDebtsUseCase makeGetSumDebtsUseCase() {
+  DebtRepository debtRepository = makeDebtRepository();
+  GetSumDebtsUseCase getSumDebtsUseCase = GetSumDebtsUseCase(debtRepository);
+  return getSumDebtsUseCase;
+}
+
+DebtPage makeDebtPage() {
+  DebtRepository debtRepository = makeDebtRepository();
 
   CreateDebtUseCase createDebtUseCase = CreateDebtUseCase(debtRepository);
   AddDebtValueUseCase addDebtValueUseCase = AddDebtValueUseCase(debtRepository);
-  GetSumDebtsUseCase getSumDebtsUseCase = GetSumDebtsUseCase(debtRepository);
-  GetDebtsUseCase getDebtsUseCase = GetDebtsUseCase(debtRepository);
+  GetSumDebtsUseCase getSumDebtsUseCase = makeGetSumDebtsUseCase();
+  GetDebtsUseCase getDebtsUseCase = makeGetDebtsUseCase();
 
   DebtController debtController = DebtController(
     addDebtValueUseCase: addDebtValueUseCase,
