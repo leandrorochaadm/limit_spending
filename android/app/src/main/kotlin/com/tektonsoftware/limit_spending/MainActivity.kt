bed1e42879d5436a0b188d9db29c14e25b632adf
LeandroRocha <leandrorochaadm@gmail.com>
2025-12-20 22:53:00 -0300

    chore: migrate from android/ios to web-only and add firebase hosting

--- android/app/src/main/kotlin/com/tektonsoftware/limit_spending/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tektonsoftware.limit_spending
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/tektonsoftware/limitspending/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.tektonsoftware.limitspending
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
